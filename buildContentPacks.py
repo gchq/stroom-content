daa212dea3ba3ddcbf6f754b0521272f58d80918
at055612 <at055612@gmail.com>
2017-07-20 15:30:04 +0100

    Change print statements to work with python 3

--- buildContentPacks.py
+++ buildContentPacks.py
@@ -44,7 +44,7 @@ import shutil
 import fnmatch
 import xml.etree.ElementTree as ET
 
-USAGE_TXT = "Usage:\nbuildContentPacks.py [--combine] [--all] [packName ...]\ne.g.\nTo build all content packs - buildPacks.py [--combine] --all\nTo build specific named content packs - buildPacks.py [--combine] pack-1 pack-2 pack-n\nWhere --combine indicates all packs should be combined into a single zip file, otherwise each pack will be placed in its own zip file."
+USAGE_TXT = "Usage:\nbuildContentPacks.py [--combine] [--validateOnly] [--all] [packName ...]\ne.g.\nTo build all content packs - buildPacks.py [--combine] --all\nTo build specific named content packs - buildPacks.py [--combine] pack-1 pack-2 pack-n\nWhere --combine indicates all packs should be combined into a single zip file, otherwise each pack will be placed in its own zip file."
 SOURCE_DIR_NAME = "source"
 TARGET_DIR_NAME = "target"
 STROOM_CONTENT_DIR_NAME = "stroomContent"
@@ -62,14 +62,14 @@ def list_content_packs():
             print("  " + the_file)
 
 def print_usage():
-    print USAGE_TXT
-    print "\nAvailable content packs:"
+    print(USAGE_TXT)
+    print("\nAvailable content packs:")
     list_content_packs()
-    print "\n"
+    print("\n")
 
 
 def clear_dir(dir_path):
-    print "Clearing contents of %s" % (dir_path)
+    print("Clearing contents of {}").format(dir_path)
     for the_file in os.listdir(dir_path):
         file_path = os.path.join(dir_path, the_file)
         try:
@@ -92,14 +92,14 @@ def zip_pack(root_path, pack_name, zip_handle, added_files):
             rel_path = os.path.relpath(abs_path, zip_source_path)
             if not rel_path in added_files:
                 added_files.append(rel_path)
-                print "  %s" % (rel_path)
+                print("  {}").format(rel_path)
                 zip_handle.write(abs_path, rel_path)
 
 
 def build_pack(pack_name):
     dest_zip_file_name = pack_name + ".zip"
     dest_zip_file_path = os.path.join(target_path, dest_zip_file_name)
-    print "Building content pack: %s into zip file %s" % (pack_name, dest_zip_file_name)
+    print("Building content pack: {} into zip file {}").format(pack_name, dest_zip_file_name)
 
     added_files = []
 
@@ -108,10 +108,10 @@ def build_pack(pack_name):
 
 def extract_uuid(entity_file):
     if not os.path.isfile(entity_file):
-        print "ERROR - Entity file %s does not exist" % entity_file
+        print("ERROR - Entity file {} does not exist").format(entity_file)
         exit(1)
 
-    # print "Extracting uuid for %s" % entity_file
+    # print("Extracting uuid for {}").format(entity_file)
     xml_root = ET.parse(entity_file).getroot()
     uuidElm = xml_root.find('uuid')
 
@@ -123,10 +123,10 @@ def extract_uuid(entity_file):
 
 def extract_entity_type(entity_file):
     if not os.path.isfile(entity_file):
-        print "ERROR - Entity file %s does not exist" % entity_file
+        print("ERROR - Entity file {} does not exist").format(entity_file)
         exit(1)
 
-    # print "Extracting uuid for %s" % entity_file
+    # print("Extracting uuid for {}").format(entity_file)
     xml_root = ET.parse(entity_file).getroot()
     entity_type = xml_root.tag
 
@@ -136,7 +136,7 @@ def extract_entity_type(entity_file):
 
 def validate_packs(pack_list, root_path):
 
-    # print "Validating packs: %s" % pack_list
+    # print("Validating packs: {}").format(pack_list)
     
     path_to_uuid_dict = dict()
     uuids = []
@@ -144,7 +144,7 @@ def validate_packs(pack_list, root_path):
         pack_path = os.path.join(root_path, pack)
         #check the folder exists for the pack name
         if not os.path.isdir(pack_path):
-            print "ERROR - Pack %s does not exist in %s" % (pack, root_path)
+            print("ERROR - Pack {} does not exist in {}").format(pack, root_path)
             exit(1)
 
         stroom_content_path = os.path.join(pack_path, STROOM_CONTENT_DIR_NAME)
@@ -153,24 +153,24 @@ def validate_packs(pack_list, root_path):
         #different uuids else this may cause odd behaviour on import
         for root, dirnames, filenames in os.walk(stroom_content_path):
             # folder_entities = fnmatch.filter(filenames, '*' + FOLDER_ENTITY_SUFFIX) 
-            # print "folder entities: %s" % folder_entities
+            # print("folder entities: {}").format(folder_entities)
             # for filename in folder_entities:
             for dirname in dirnames:
-                # print "dirname: %s" % dirname
+                # print("dirname: {}").format(dirname)
                 full_filename = os.path.join(root, dirname, '..', dirname + FOLDER_ENTITY_SUFFIX)
-                # print "full_filename: %s" % full_filename
+                # print("full_filename: {}").format(full_filename)
                 entity_path = os.path.relpath(os.path.join(root, dirname), stroom_content_path)
-                # print "entity_path: %s" % entity_path
+                # print("entity_path: {}").format(entity_path)
                 uuid = extract_uuid(full_filename)
                 if uuid == None:
-                    print "ERROR - Entity file %s does not have a UUID" % full_filename
+                    print("ERROR - Entity file {} does not have a UUID").format(full_filename)
                     exit(1)
-                # print "uuid = %s" % uuid
+                # print("uuid = {}").format(uuid)
 
                 if not entity_path in path_to_uuid_dict:
                     path_to_uuid_dict[entity_path] = uuid
                 elif path_to_uuid_dict[entity_path] != uuid:
-                    print "ERROR - Multiple uuids exist for path %s" % entity_path
+                    print("ERROR - Multiple uuids exist for path {}").format(entity_path)
                     exit(1)
 
 
@@ -188,18 +188,18 @@ def validate_packs(pack_list, root_path):
                     if entity_type != FOLDER_ENTITY_TYPE:
                         #this is not a folder entity
                         if uuid in uuids:
-                            print "ERROR - Entity %s with type %s has a duplicate UUID %s" % (full_filename, entity_type, uuid)
+                            print("ERROR - Entity {} with type {} has a duplicate UUID {}").format(full_filename, entity_type, uuid)
                             exit(1)
                         else:
                             uuids.append(uuid)
 
-    print ""
-    print "UUIDs for paths:"
+    print("")
+    print("UUIDs for paths:")
     for key in sorted(path_to_uuid_dict):
-        print "%s - %s" % (key, path_to_uuid_dict[key])
+        print("{} - {}").format(key, path_to_uuid_dict[key])
 
-    print ""
-    print "Validation completed with no errors"
+    print("")
+    print("Validation completed with no errors")
 
 
 
@@ -208,7 +208,7 @@ def validate_packs(pack_list, root_path):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if len(sys.argv) == 1:
-    print "ERROR - No arguments supplied"
+    print("ERROR - No arguments supplied")
     print_usage()
     exit(1)
 
@@ -228,31 +228,31 @@ for arg in sys.argv[1:]:
         packs_to_build.append(arg)
 
 if len(packs_to_build) > 0 and isAllPacks:
-    print "ERROR - Cannot specify --all and named packs"
+    print("ERROR - Cannot specify --all and named packs")
     print_usage()
     exit(1)
 
 if len(packs_to_build) == 0 and not isAllPacks:
-    print "ERROR - Must specify --all or provide a list of named packs"
+    print("ERROR - Must specify --all or provide a list of named packs")
     print_usage()
     exit(1)
 
 # ensure we have a target dir to put the zips in 
 if not os.path.exists(target_path):
-    print "Creating target directory %s" % target_path
+    print("Creating target directory {}").format(target_path)
     os.mkdir(target_path)
 
 if isAllPacks:
-    print "Processing all content packs"
+    print("Processing all content packs")
     for list_entry in os.listdir(source_path):
         if os.path.isdir(os.path.join(source_path, list_entry)):
             packs_to_build.append(list_entry)
 else:
-    print "Processing packs: %s" % packs_to_build
+    print("Processing packs: {}").format(packs_to_build)
 
-print "Using root path: ", root_path
-print "Using source path: ", source_path
-print "Using target path: ", target_path
+print("Using root path: ").format(root_path)
+print("Using source path: ").format(source_path)
+print("Using target path: ").format(target_path)
 
 validate_packs(packs_to_build, source_path)
 
@@ -270,5 +270,5 @@ if not isValidateOnly:
         for pack in packs_to_build:
             build_pack(pack)
 
-print "Done!"
+print("Done!")
 exit(0)
