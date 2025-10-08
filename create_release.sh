2823820816255476e56d65a7e314cc1173d63898
at055612 <22818309+at055612@users.noreply.github.com>
2025-10-08 15:16:36 +0100

    Add ver to zip filename in create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-
 set -e
+
+# Set up a trap to catch SIGINT (Ctrl+C) and execute the cleanup function
+trap cleanup SIGINT
+
+cleanup() {
+  if [[ -n "${TEMP_DIR}" ]] && [[ -d "${TEMP_DIR}" ]]; then
+    rm -rf "${TEMP_DIR:?TEMP_DIR not set}"
+  fi
+}
 
 setup_echo_colours() {
   # Exit the script on any error
@@ -56,6 +64,7 @@ validate_for_uncommitted_work() {
 main() {
   IS_DEBUG=false
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+  TEMP_DIR=
 
   setup_echo_colours
 
@@ -64,8 +73,10 @@ main() {
   local notes="${1?notes argument must be supplied, e.g. "Adds feature X"}"; shift
   local tag="${pack_name}-${version}"
   local dist_sub_dir="build/distributions"
-  local zip_file="${dist_sub_dir}/${pack_name}.zip"
-  local zip_all_file="${dist_sub_dir}/${pack_name}-all.zip"
+  local zip_filename="${pack_name}.zip"
+  local zip_all_filename="${pack_name}-all.zip"
+  local source_zip_file="${dist_sub_dir}/${zip_filename}"
+  local source_zip_all_file="${dist_sub_dir}/${zip_all_filename}"
 
   validate_for_uncommitted_work
 
@@ -78,40 +89,48 @@ main() {
 
   ./gradlew clean build
 
-  if [[ ! -f "${zip_file}" ]]; then
-    echo "Can't find content pack zip ${zip_file}" >&2
+  if [[ ! -f "${source_zip_file}" ]]; then
+    echo "Can't find content pack zip ${source_zip_file}" >&2
     exit 1
   fi
 
-  if [[ ! -f "${zip_all_file}" ]]; then
-    echo "Can't find content pack zip ${zip_all_file}" >&2
+  if [[ ! -f "${source_zip_all_file}" ]]; then
+    echo "Can't find content pack zip ${source_zip_all_file}" >&2
     exit 1
   fi
+  TEMP_DIR=$( mktemp -d )
 
+  local temp_zip_file="${TEMP_DIR}/${pack_name}-${version}.zip"
+  cp "${source_zip_file}" "${temp_zip_file}"
 
   local extra_args=()
-  extra_args+=( "${zip_file}" )
+  extra_args+=( "${temp_zip_file}" )
 
   # No point including the -all file if it is the same
-  if cmp -s "${zip_file}" "${zip_all_file}"; then
-    echo -e "${GREEN}Skipping identical asset ${BLUE}${zip_all_file}${NC}"
+  if cmp -s "${source_zip_file}" "${source_zip_all_file}"; then
+    echo -e "${GREEN}Skipping identical asset ${BLUE}${zip_all_filename}${NC}"
   else
-    extra_args+=( "${zip_all_file}" )
+    local temp_zip_all_file="${TEMP_DIR}/${pack_name}-all-${version}.zip"
+    cp "${source_zip_all_file}" "${temp_zip_all_file}"
+    extra_args+=( "${temp_zip_all_file}" )
   fi
 
   echo -e "${GREEN}Creating release for pack ${BLUE}${pack_name}${GREEN}" \
     "with tag ${BLUE}${tag}${GREEN} and assets:${NC}"
 
   for file in "${extra_args[@]}"; do
-    echo -e "  ${BLUE}${file}${NC}"
+    # Stip the path bit off to leave just filename
+    echo -e "  ${BLUE}${file##*/}${NC}"
   done
 
+  echo
   read -rsp $'Press "y" to continue, any other key to cancel.\n' -n1 keyPressed
 
   if [ "$keyPressed" != 'y' ] && [ "$keyPressed" != 'Y' ]; then
     echo
     echo -e "${GREEN}Exiting without creating a release.${NC}"
     echo
+    cleanup
     exit 0
   fi
   
@@ -122,6 +141,7 @@ main() {
     "${extra_args[@]}"
   
   popd >/dev/null
+  cleanup
   echo -e "${GREEN}Done${NC}"
 }
 
